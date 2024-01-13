6f6803ad106d3ceebe129d09c4b6d087bf9e5a46
mrjoe99 <57552721+mrjoe99@users.noreply.github.com>
2024-01-13 00:34:10 -0500

    Update advent_5.py

--- advent_5.py
+++ advent_5.py
@@ -4,21 +4,25 @@ ABSOLUTE_PATH = os.path.dirname(__file__)
 DAY = os.path.basename(__file__).split('_')[1].split(".")[0]
 EXAMPLE = True
 
-class AlmanacMaps(object):
-
+class Maps(object):
     def __init__(self, name, dest_ranges, source_ranges, range_lengths):
-        name = name
+        self.name = name
         self._dest_ranges = dest_ranges
         self._source_ranges = source_ranges
         self._range_lengths = range_lengths
-        self._maps = {}
+        self._custom_map = []
+        self._populate_map()
 
-        self._populate_maps()
+    def _populate_map(self):
+        for x in range(self.name[2], self.name[1]): # index range of map
+            source = self._source_ranges[x]
+            destination = self._dest_ranges[x]
+            range_length = range_lengths[x]
 
-    def _populate_maps(self):
-        for x in range(0, len(self._dest_ranges)):
-            for y in range(0, self._range_lengths):
-                print("DEBUG")
+            for y in range(0, range_length):
+                self._custom_map.append([source + y, destination + y])
+
+        self.name = self.name[0]
 
 
 def get_input():
@@ -36,37 +40,37 @@ def get_input():
     return input
 
 def parse_input(input):
-    seeds = []
-    names = []
-    destinations = []
-    sources = []
-    ranges = []
+        seeds = []
+        names = []
+        destinations = []
+        sources = []
+        ranges = []
 
-    seeds = [int(x) for x in input[0][7:].split(" ")]
+        seeds = [int(x) for x in input[0][7:].split(" ")]
 
-    for x in range(2, len(input)):
-        if "map" in input[x]:
-            names.append([input[x][:-5], 0])
-        elif input[x] != '':
-            temp = input[x].split(" ")
+        counter = 0
+        for x in range(2, len(input)):
+            if "map" in input[x]:
+                names.append([input[x][:-5], 0, counter]) # name, range, starting index
+            elif input[x] != '':
+                temp = input[x].split(" ")
 
-            destinations.append(int(temp[0]))
-            sources.append(int(temp[1]))
-            ranges.append(int(temp[2]))
-            names[len(names) - 1][1] += 1
+                destinations.append(int(temp[0]))
+                sources.append(int(temp[1]))
+                ranges.append(int(temp[2]))
+                names[len(names) - 1][1] += 1
+                counter += 1
 
-    print("DEBUG")
-    return seeds, names, destinations, sources, ranges
-
-
+        return seeds, names, destinations, sources, ranges    
 
 
 if __name__ == '__main__':
     input = get_input()
     seeds, names, dest_ranges, source_ranges, range_lengths = parse_input(input)
+    maps_collection = {}
+    
+    for name in names:
+        maps_collection[name[0]] = Maps(name, dest_ranges, source_ranges, range_lengths)
 
     
-    test = AlmanacMap("test", [], [], [])
-
-    test.name
     print("DEBUG")
