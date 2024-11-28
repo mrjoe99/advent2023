8f7ad89949ff2049d360aff0260cba4b822395fe
mrjoe99 <57552721+mrjoe99@users.noreply.github.com>
2024-11-28 11:00:06 -0500

    completed day 1

--- advent_1.py
+++ advent_1.py
@@ -1,18 +1,28 @@
 import os
+import re
+from functools import reduce
 
 ABSOLUTE_PATH = os.path.dirname(__file__)
 DAY = 1
-EXAMPLE = True
 
-def get_input():
-    file = ""
-    temp = ""
+digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+words = ["zero", "one", "two", "three", "four", "five", "six", "seven",
+             "eight", "nine"]
+word_conversions = {
+        "zero": '0',
+        "one": '1',
+        "two": '2',
+        "three": '3',
+        "four": '4',
+        "five": '5',
+        "six": '6',
+        "seven": '7',
+        "eight": '8',
+        "nine": '9'
+    }
 
-    if EXAMPLE:
-        file = f'{ABSOLUTE_PATH}/example_advent_data_{DAY}.txt'
-    else:
-        file = f'{ABSOLUTE_PATH}/advent_data_{DAY}.txt'
 
+def get_input(file):
     with open(file, 'r') as input:
         temp = []
 
@@ -23,147 +33,38 @@ def get_input():
     
 def extract_numerical_calibration_values(input):
     values = []
-    digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     for line in input:
-        line_values = []
+        line_values = {}
 
         for x in range(0, len(line)):
             if line[x] in digits:
-                line_values.append([line[x], x]) # [value, starting index]
+                if line[x] not in line_values.keys():
+                    line_values[line[x]] = []
+
+                line_values[line[x]].append(x)
 
         values.append(line_values)
 
     return values
 
 def find_text(line):
-    values = []
-    current_index = 0
-    #iteration = 0
-
-    three_slice = line[current_index: current_index + 3] # 3 letter words: one, two, six
-    four_slice = line[current_index: current_index + 4]  # 4 letter words: four, five, nine
-    five_slice = line[current_index: current_index + 5]  # 5 letter words: three, seven, eight
-
-    while current_index <= len(line):
-        # if iteration > 0:
-        #     current_index += 1
-        #     iteration = 0
+    matches = {}
+    
+    for word in words:
+        # using re.finditer() to find all occurrences of substring in string 
+        occurrences = re.finditer(word, line) 
         
-            # three_slice = line[current_index: current_index + 3]
-            # four_slice = line[current_index: current_index + 4]
-            # five_slice = line[current_index: current_index + 5]
+        # using reduce() to get start indices of all occurrences 
+        res = reduce(lambda x, y: x + [y.start()], occurrences, []) 
+        if res != []:
+            if word not in matches.keys():
+                matches[word] = []
 
-        if three_slice == "one": 
-            index = line.find("one", current_index)
-            #current_index += 3 # (length of the word 'one')
+            for item in res:
+                matches[word].append(item)
 
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["1", index])
-        elif three_slice == "two":
-            index = line.find("two", current_index)
-            #current_index += 3 # (length of the word 'two')
-
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["2", index])
-        elif three_slice == "six":
-            index = line.find("six", current_index)
-            #current_index += 3 # (length of the word 'six')
-
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["6", index])
-        
-        if four_slice == "four": 
-            index = line.find("four", current_index)
-            #current_index += 4 # (length of the word 'four')
-
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["4", index])
-        elif four_slice == "five":
-            index = line.find("five", current_index)
-            #current_index += 4 # (length of the word 'five')
-
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["5", index])
-        elif four_slice == "nine":
-            index = line.find("nine", current_index)
-            #current_index += 4 # (length of the word 'nine')
-
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["9", index])
-
-        if five_slice == "three": 
-            index = line.find("three", current_index)
-            #current_index += 5 # (length of the word 'three')
-
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["3", index])
-        elif five_slice == "seven":
-            index = line.find("seven", current_index)
-            #current_index += 5 # (length of the word 'seven')
-
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["7", index])
-        elif five_slice == "eight":
-            index = line.find("eight", current_index)
-            #current_index += 5 # (length of the word 'eight')
-
-            three_slice = line[current_index: current_index + 3]
-            four_slice = line[current_index: current_index + 4]
-            five_slice = line[current_index: current_index + 5]
-
-            if index == -1:
-                break
-            values.append(["8", index])
-        
-        # if (three_slice != "one" and three_slice != "two" and three_slice != "six" and # nudge 'current_index' forward by 1 if word not matched
-        #     four_slice != "four" and four_slice != "five" and four_slice != "nine" and
-        #     five_slice != "three" and five_slice != "seven" and five_slice != "eight"):
-        #     iteration += 1
-        
-        current_index += 1
-
-    return values
+    return matches
 
 def extract_text_calibration_values(input):
     values = []
@@ -178,8 +79,15 @@ def combine_values(numerical_values, text_values):
     values = []
 
     for x in range(0, len(numerical_values)):
-        values.append(numerical_values[x])
-        values[x].extend(text_values[x])
+        values.append([])
+
+        for item in numerical_values[x]:
+            for value in numerical_values[x][item]:
+                values[x].append([item, value])
+        
+        for item in text_values[x]:
+            for value in text_values[x][item]:
+                values[x].append([item, value])
 
     return values
 
@@ -191,36 +99,53 @@ def order_values(values):
 
     return sorted_values
 
-def convert_to_integers(sorted):
+def convert_to_strings(input):
     values = []
 
-    for item in sorted:
-        values.append(int((item[0][0] + item[len(item) - 1][0])))
+    try:
+        for x, line in enumerate(input):
+            values.append([])
+            for item in line:
+                if item[0] in digits:
+                    values[x].append(int(item[0]))
+                elif item[0] in words:
+                    values[x].append(int(word_conversions[item[0]]))
+                else:
+                    raise Exception
+    except Exception:
+        print("Unexpected value")
 
     return values
 
+def combine_first_and_last_digit(values):
+    numbers = []
+
+    for line in values:
+        numbers.append(int(f"{line[0]}{line[-1]}"))
+
+    return numbers
+
 def sum_and_print_values(values):
-    sum = int()
+    sum = 0
 
     for value in values:
         sum += value
 
-    print(sum)
+    print(f"SUM: {sum}")
 
 
 if __name__ == '__main__':
-    input = get_input()
+    file = f'{ABSOLUTE_PATH}/advent_data_{DAY}.txt'
+    #file = f'{ABSOLUTE_PATH}/example_advent_data_{DAY}.txt'
+    
+    input = get_input(file)
 
     numerical_values = extract_numerical_calibration_values(input)
     text_values = extract_text_calibration_values(input)
 
-    combined_values = combine_values(numerical_values, text_values)
-    ordered_values = order_values(combined_values)
+    values = combine_values(numerical_values, text_values)
+    values = order_values(values)
 
-    integer_values = convert_to_integers(ordered_values)
-    sum = sum_and_print_values(integer_values)
-
-    print("DEBUG")
-
-# 53587 - too low
-# 54494 - too high
+    values = convert_to_strings(values)
+    values = combine_first_and_last_digit(values)
+    sum = sum_and_print_values(values)
